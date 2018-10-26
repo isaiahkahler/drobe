a7b3261cd846861a51abe19aef1945f3caccbcdd
Isaiah Kahler <isaiahkahler@thicc-mac.local>
2018-10-26 01:46:15 -0400

    show hide sidebar sections

--- pages/library/sortSidebar.tsx
+++ pages/library/sortSidebar.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { View, Text, TouchableHighlight, StyleSheet, Dimensions, SectionList } from 'react-native';
+import { View, Text, TouchableHighlight, StyleSheet, Dimensions, SectionList, ScrollView } from 'react-native';
 import { commonStyles } from '../../components/styles';
 import { ItemDefinitions } from '../../components/formats';
 
@@ -7,7 +7,7 @@ const width = Dimensions.get('screen').width;
 const height = Dimensions.get('screen').height;
 
 interface SortSidebarProps {
-  onSelect(type: string, value: any): Function;
+  onSelect: Function;
 }
 
 interface SortSidebarState {
@@ -24,55 +24,127 @@ export class SortSidebar extends React.Component<SortSidebarProps, SortSidebarSt
   render() {
     return (
       <View style={styles.sidebar}>
-        {/* <SectionList
-            sections={[
-              { title: 'D', data: ['Devin'] },
-              { title: 'J', data: ['Jackson', 'James', 'Jillian', 'Jimmy', 'Joel', 'John', 'Julie'] }
-            ]}
-            renderItem={({ item }) => <Text style={[styles.listItem, commonStyles.pb]}>{item}</Text>}
-            renderSectionHeader={({ section }) => (
-              <Text style={[styles.listSectionHeader, commonStyles.h2]}>{section.title}</Text>
-            )}
-            keyExtractor={(item, index) => index}
-          /> */}
-          <Text style={[commonStyles.h2, commonStyles.bold]}>sort by</Text>
-        <Section label="date" options={['new to old', 'old to new']} action={(index) => { this.props.onSelect("date", index) }} />
-        <Section label="class" options={ItemDefinitions.classes} action={(index) => { this.props.onSelect("class", index) }} />
+        <ScrollView>
+          <Text style={[commonStyles.h1, commonStyles.bold]}>sort by</Text>
+          <Section label="date" options={['new to old', 'old to new']} action={(index) => { this.props.onSelect("date", index) }} />
+          <Section label="type" options={[
+            { sublabel: "tops", options: ItemDefinitions.types.top },
+            { sublabel: "bottoms", options: ItemDefinitions.types.bottom },
+            { sublabel: "full bodies", options: ItemDefinitions.types.full },
+            { sublabel: "shoes", options: ItemDefinitions.types.shoes },
+            { sublabel: "accessories", options: ItemDefinitions.types.accessory },
+          ]} action={(type, index) => { this.props.onSelect(type, index) }} />
+        </ScrollView>
       </View>
     );
   }
 }
 
-function Section(props: { label: string, options: Array<String>, action: Function }) {
-  return (
-    <View style={styles.section}>
-      <TouchableHighlight>
-        <Text style={[commonStyles.pb, commonStyles.bold]}>{props.label}</Text>
-      </TouchableHighlight>
-      <View style={styles.subsection}>
+interface SectionProps {
+  label: string, options: Array<string | { sublabel: string, options: Array<string> }>, action: Function
+}
 
-        {props.options.map((item, index) => {
-          return (
-            <TouchableHighlight onPress={() => props.action()} key={index}><Text style={commonStyles.pb}>{item}</Text></TouchableHighlight>
-          );
-        })}
+interface SectionState {
+  show: boolean;
+}
+
+class Section extends React.Component<SectionProps, SectionState> {
+  constructor(props: SectionProps) {
+    super(props);
+    this.state = { show: false }
+  }
+
+  toggleList = () => {
+    this.setState(previousState => ({
+      ...previousState,
+      show: !previousState.show
+    }))
+  }
+
+  render() {
+    return (
+      <View style={styles.section} >
+        <TouchableHighlight onPress={this.toggleList} underlayColor='rgba(0,0,0,0.1)' >
+          <Text style={[commonStyles.h2, commonStyles.bold]}>{this.props.label}</Text>
+        </TouchableHighlight>
+        {this.state.show && (
+          <View style={styles.optionLabel}>
+
+            {this.props.options.map((item, index) => {
+              if (typeof item === "string") {
+                return (
+                  <TouchableHighlight onPress={() => this.props.action(index)} key={index} underlayColor='rgba(0,0,0,0.1)' ><Text style={commonStyles.h2}>{item}</Text></TouchableHighlight>
+                );
+              } else {
+                return (
+                  <Subsection label={item.sublabel} options={item.options} action={this.props.action} key={index} />
+                );
+              }
+            })}
+          </View>
+        )}
       </View>
-    </View>
-  );
+    );
+  }
+}
+
+interface SubsectionProps {
+  label: string, options: string[], action: Function
+}
+
+interface SubsectionState {
+  show: boolean;
+}
+
+
+class Subsection extends React.Component<SubsectionProps, SubsectionState> {
+  constructor(props: SubsectionProps) {
+    super(props);
+    this.state = { show: false }
+  }
+
+  toggleList = () => {
+    this.setState(previousState => ({
+      ...previousState,
+      show: !previousState.show
+    }))
+  }
+
+  render() {
+    return (
+      <View style={styles.subsection}>
+        <TouchableHighlight onPress={this.toggleList} underlayColor='rgba(0,0,0,0.1)' >
+          <Text style={[commonStyles.h2, commonStyles.bold]}>{this.props.label}</Text>
+        </TouchableHighlight>
+        {this.state.show && (
+          <View style={styles.optionLabel}>
+            {this.props.options.map((item, index) => {
+              return (
+                <TouchableHighlight key={index} onPress={() => this.props.action(this.props.label, index)} underlayColor='rgba(0,0,0,0.1)' >
+                  <Text style={commonStyles.pb}>{item}</Text>
+                </TouchableHighlight>);
+            })}
+          </View>
+        )}
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   sidebar: {
     width: width * 0.8,
-    height: height,
+    flex: 1,
     padding: 10,
     backgroundColor: '#E9E9EF'
   },
   section: {
-    borderTopWidth: 2
-
+    borderTopWidth: 2,
+    paddingVertical: 5
   },
-  subsection: {
+  optionLabel: {
     paddingLeft: 20,
-  }
+    paddingTop: 2
+  },
+  subsection: {},
 });
